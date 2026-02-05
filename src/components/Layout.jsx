1fbef8d411af4f6d023587ba51fe2d70a1dea7fb
UttamMoradiya <umoradiya31@gmail.com>
2026-02-05 18:53:08 +0530

    Refector analytics

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import { useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
+import { Analytics } from "@vercel/analytics/next"
 
 const Layout = ({ children }) => {
     const { pathname } = useLocation();
@@ -18,6 +19,7 @@ const Layout = ({ children }) => {
                 {children}
             </main>
             <Footer />
+            <Analytics />
         </div>
     );
 };
